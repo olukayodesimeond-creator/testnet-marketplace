372cf8d523771834d537a4e6a7f6ba15a9d7ee3b
olukayodesimeond-creator <olukayodesimeond@gmail.com>
2025-10-21 10:45:33 +0100

    Created sample Solidity file for contract structure

---
+++ contracts/sample.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity ^0.8.0;
+
+// Sample contract placeholder for Testnet Marketplace
+contract TestnetRegistry {
+    string public projectName = "Testnet Marketplace";
+}
